ebc4b34a4507c1263c3b9ac15f956203fc3d8546
MikeShahin <gobias101@gmail.com>
2021-05-16 19:37:14 -0700

    added create playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,30 @@
 class PlaylistsController < ApplicationController
+    def index
+        playlists = Playlist.all
+        render json: playlists
+    end
+
+    def show
+        @playlist = Playlist.find_by(id: params[:id])
+        if @playlist
+            render json: @playlist
+        else 
+            render json: {error: "playlist doesn't exist"}
+        end
+    end
+
+    def create
+        @playlist = Playlist.create(playlist_params)
+        if @playlist
+            render json: {playlist: @playlist, message: "created!"}
+        else
+            render json: {error: "something went wrong, playlist not saved!"}
+        end
+    end
+
+    private
+
+    def playlist_params
+        params.require(:playlist).permit(:name)
+    end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,32 @@
+require 'pry'
 class SongsController < ApplicationController
+
+    def index
+        @songs = Song.all
+        render json: @songs
+    end
+
+    def show
+        @song = Song.find_by(id: params[:id])
+        if @song
+            render json: @song
+        else
+            render json: {error: "nothing found"}
+        end
+    end
+
+    def create
+        @song = Song.create(song_params)
+        if @song
+            render json: {song: @song, message: "created!"}
+        else
+            render json: {error: "something went wrong, song not saved!"}
+        end
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:artist, :songName, :preview, :playlist_id)
+    end
 end
